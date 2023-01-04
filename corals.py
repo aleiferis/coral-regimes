b7ce9efe9fe8cb38667b6ec6cc4accd42bd11c86
Spyros <aleiferis@gmail.com>
2023-01-04 15:54:35 +0200

    Import specie codes

--- corals.py
+++ corals.py
@@ -4,8 +4,8 @@ import sys
 import pandas as pd
 
 benthic = pd.read_csv('./benthic.csv',skiprows=[0])
-human = pd.read_csv('./human.csv',skiprows=[0])
-
+human = pd.read_csv('./human.csv')
+species_codes = pd.read_csv('./species.csv')
 
 
 
