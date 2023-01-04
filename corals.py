577b4449300a8add194f4f8b3574701036507a60
Spyros <aleiferis@gmail.com>
2023-01-04 10:12:26 +0200

    Import data

--- corals.py
+++ corals.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
 import sys
+import pandas as pd
 
-print('hello')
+benthic = pd.read_csv('./benthic.csv',skiprows=[0])
+human = pd.read_csv('./human.csv',skiprows=[0])
+
+
+
+
